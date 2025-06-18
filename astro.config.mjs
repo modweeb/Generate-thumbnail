94555e138ea136dfbe3784fa8eb545f5e49d5f57
مود ويب <modweeb3@gmail.com>
2025-06-19 02:44:19 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,7 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   site: "https://modweeb.com",
-  base: "/generate-thumbnail",
-  integrations: [sitemap()],
-  output: 'server',
-  adapter: vercel(),
-  vite: {
-    define: {
-      'import.meta.env.BASE_URL': JSON.stringify('/generate-thumbnail')
-    }
-  }
+  integrations: [sitemap()]
 });
