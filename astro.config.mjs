e658bf87bb21a8b50630f0b1c2ff847a25ffea99
مود ويب <modweeb3@gmail.com>
2025-06-19 02:46:16 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+// https://astro.build/config
 export default defineConfig({
-  site: "https://modweeb.com",
+  site: "https://modweeb.com", // بدون الشرطة المائلة في النهاية
   integrations: [sitemap()]
 });
